7ee12161d4e38f274de3b540b5cc9a36c39be03c
toandv <toandv@users.noreply.github.com>
2017-03-27 12:12:01 +0700

    Update mt_book-summaries.py

--- mt_book-summaries.py
+++ mt_book-summaries.py
@@ -1,10 +1,10 @@
 
 # A multi-threaded script to download all book summaries from https://paulminors.com/resources/book-summaries
 # dependencies: 
-    # pip install beautifulsoup4
+#   pip install beautifulsoup4
 # docs:    
-    # https://www.crummy.com/software/BeautifulSoup/bs4/doc
-    # https://docs.python.org/3/library/queue.html
+#   https://www.crummy.com/software/BeautifulSoup/bs4/doc
+#   https://docs.python.org/3/library/queue.html
 
 import urllib.request
 from bs4 import BeautifulSoup
